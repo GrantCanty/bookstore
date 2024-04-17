021cfe10fdfa740af7109210176a01ee6a64ca9b
GrantCanty <gc933815@ohio.edu>
2024-04-16 21:41:28 -0600

    updated home.js to support search functionality. when enter is pressed, fetchSearchResults is called, taking the value of the search box. if the search box is empty, it will call all available books. if no result, this is displayed in the table

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -5,24 +5,41 @@ import '../styles/home.css'
 
 const Home = () => {
     const [data, setData] = useState([])
-    const [searchData, setSearchData] = useState("")
+    const [searchData, setSearchData] = useState('')
 
     
 
     useEffect(() => {
-        fetch(`http://localhost:8080/api/content/available`)   
+        fetch('http://localhost:8080/api/content/available')   
         .then(data => data.json())
         .then(json => setData(json))
         .catch(error => console.error(error))
     },[])
 
-    //console.log(data)
+    const fetchSearchResults = (searchParam) => {
+        
+        (searchParam === '' ? 
+        fetch('http://localhost:8080/api/content/available')
+        .then(data => data.json())
+        .then(json => setData(json))
+        .catch(error => console.error(error))
+        :
+        fetch('http://localhost:8080/api/content/' + searchParam)
+        .then(data => data.json())
+        .then(json => setData(json))
+        .catch(error => console.error(error)))
+
+        setSearchData('')
+    }
+
+    /*console.log("data: ", data)
+    console.log("data: ", data.length)*/
     
     
     return(
         <div className='content'>
             <div className='search-area'>
-                <input type='text' name='search' placeholder="Search by Author or Book Name" value={searchData} onChange={ (e) => setSearchData(e.target.value) }/>
+                <input onKeyDown={(e) => e.key === 'Enter' ? fetchSearchResults(searchData) : null } type='text' name='search' placeholder="Search by Author or Book Name" value={searchData} onChange={ (e) => setSearchData(e.target.value) }/>
                 <button>New</button>
             </div>
             <div className='content-table'>
@@ -32,7 +49,9 @@ const Home = () => {
                     <span className='item-info'>Available Copies</span>
                     <span className='item-info'>Edit</span>
                 </div>
-                {data.map((e) => {
+                
+                {data.length == 0 ?  <h3>No Results</h3> :
+                data.map((e) => {
                 return <TableItem key={e.id} idName={'content'} title={e.title} author={e.authorName} available={e.inventoryCount} />
             })}
             </div>
